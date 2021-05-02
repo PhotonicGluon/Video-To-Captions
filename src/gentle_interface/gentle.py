d907ffd5fa470b084b26071b32a46b3cafc42a58
Ryan-Kan <25820201+Ryan-Kan@users.noreply.github.com>
2021-05-02 11:15:53 +0800

    Update Gentle Interface to Return the Words Timetable
    Fixed the progress bar code as well.

--- src/gentle_interface/gentle.py
+++ src/gentle_interface/gentle.py
@@ -2,7 +2,7 @@
 gentle.py
 
 Created on 2021-04-28
-Updated on 2021-05-01
+Updated on 2021-05-02
 
 Copyright © Ryan Kan
 
@@ -11,12 +11,14 @@ Description: The class that will directly interface with the gentle program.
 
 # IMPORTS
 import asyncio
+import math
 import os
 import re
 import subprocess
 import time
+import wave
 
-from aiohttp import ClientSession
+from aiohttp import ClientSession, ClientResponseError
 from tqdm import tqdm
 
 
@@ -50,7 +52,7 @@ class Gentle:
 
     def stop_gentle_container(self):
         """
-        Method to stop the gentle container/
+        Method to stop the gentle container.
 
         Raises:
             ModuleNotFoundError:
@@ -58,13 +60,11 @@ class Gentle:
 
         """
 
-        output = self._run_cmd("docker stop gentle-container", return_output=False)
-        print(output)
+        self._run_cmd("docker stop gentle-container", return_output=False)
 
-    def get_timetable(self, audio_file_path, transcript_path, refresh_interval=0.1):
+    def get_timetable(self, audio_file_path, transcript_path, refresh_interval=0.5, raw_timetable=None):
         """
-        Helper method that gets the timetable from the gentle server.
-        This is an asynchronous method.
+        Method that gets the timetable from the gentle server.
 
         Args:
             audio_file_path (str):
@@ -77,16 +77,32 @@ class Gentle:
                 Duration in seconds to wait before refreshing the progress bar.
                 (Default = 0.1)
 
+            raw_timetable (optional[dict]):
+                A pre-existing raw timetable.
+                (Default = None)
+
         Returns:
-            dict:
-                The timetable.
+            list[dict]:
+                The timetable which only contains the words and the times when those words were said.
 
         Raises:
             FileNotFoundError:
                 If either the audio file or the transcript cannot be found.
         """
 
-        return asyncio.run(self._get_timetable(audio_file_path, transcript_path, refresh_interval=refresh_interval))
+        # Get the raw timetable
+        if raw_timetable is None:
+            raw_timetable = self._get_raw_timetable(audio_file_path, transcript_path, refresh_interval=refresh_interval)
+
+        # Get all the words and their related data points
+        words = raw_timetable["words"]  # This is a list of dictionaries
+
+        # Remove the "phones" (phonemes) key from all the words' dictionaries
+        for word in words:
+            word.pop("phones", None)
+
+        # Return the processed timetable
+        return words
 
     # Helper Methods
     @staticmethod
@@ -134,43 +150,38 @@ class Gentle:
 
         return self._run_cmd("docker logs gentle-container --tail 1")
 
-    async def _update_progress_bar(self, refresh_interval=0.5):
+    async def _update_progress_bar(self, audio_duration, refresh_interval=0.5, chunk_len=20, overlap_t=2):
         """
         Helper method to update the progress bar on the progress of the timetable creation.
 
         Args:
+            audio_duration (float):
+                Duration of the WAV file.
+
             refresh_interval (float):
                 Duration in seconds to wait before checking for any updates.
                 (Default = 0.5)
+
+            chunk_len (int):
+                The length of each chunk.
+                Match this with Gentle's value.
+                (Default = 20)
+
+            overlap_t (int):
+                Overlap time.
+                Match this with Gentle's value.
+                (Default = 2)
         """
 
+        # Calculate the number of chunks, which is the value of `total`
+        # Note: this is based off of https://github.com/lowerquality/gentle/blob/master/gentle/transcriber.py#L20
+        total = int(math.ceil(audio_duration / float(chunk_len - overlap_t)))
+
         # Define a new progress bar object
-        progress_bar = tqdm(desc="Transcribing the Audio File")
+        progress_bar = tqdm(desc="Aligning Transcript to Audio File", total=total)
 
-        # Now continuously get command line output until something like "X/Y" is output
-        while True:
-            # Get the latest output from the command line
-            latest_output = self._get_latest_output()
-
-            # Check if it matches the format
-            match = re.search(r"(\d+)/(\d+)", latest_output)
-
-            if match:
-                # Break out of the loop
-                break
-
-            # If not; wait for another `refresh_interval` seconds before querying again
-            await asyncio.sleep(refresh_interval)
-
-        # Get the total number of iterations that needs to be run
-        total = int(match.group(2))
-
-        # Update the total on the progress bar
-        progress_bar.reset(total=total)
-        progress_bar.update(1)  # One iteration has already been run
-
-        # Continue reading from console output until `total` outputs have been received in total
-        num_outputs = 1
+        # Read from console output until `total - 1` outputs have been received in total
+        num_outputs = 0
         while num_outputs < total - 1:  # We'll need to handle the last iteration separately
             # Wait for another `refresh_interval` seconds before querying again
             await asyncio.sleep(refresh_interval)
@@ -178,20 +189,27 @@ class Gentle:
             # Get the latest output from the command line
             latest_output = self._get_latest_output()
 
-            # Get the matching group and update from there
+            # Try to get the matching group and update from there
             match = re.search(r"(\d+)/(\d+)", latest_output)
-            new_progress = int(match.group(1))  # This is the current progress
-            num_to_update_by = new_progress - num_outputs
 
-            progress_bar.update(num_to_update_by)
-            num_outputs = new_progress
+            if match:
+                # Get the current step
+                new_progress = int(match.group(1))  # This is the current step
+
+                # Calculate the value to update by
+                num_to_update_by = new_progress - num_outputs
+
+                # Update the progress bar
+                progress_bar.update(num_to_update_by)
+                num_outputs = new_progress
 
         # The last iteration would be handled here
-        progress_bar.update(2)
+        progress_bar.n = total
+        progress_bar.refresh()
 
-    async def _get_timetable(self, audio_file_path, transcript_path, refresh_interval=0.5):
+    async def _get_raw_timetable_async(self, audio_file_path, transcript_path, refresh_interval=0.5):
         """
-        Helper method that gets the timetable from the gentle server.
+        Helper method that gets the raw timetable from the gentle server.
         This is an asynchronous method.
 
         Args:
@@ -212,6 +230,9 @@ class Gentle:
         Raises:
             FileNotFoundError:
                 If either the audio file or the transcript cannot be found.
+
+            Exception:
+                If something went wrong in the gentle server.
         """
 
         # Check if the files exist
@@ -220,6 +241,10 @@ class Gentle:
         if not os.path.isfile(transcript_path):
             raise FileNotFoundError(f"The transcript cannot be found at the path '{transcript_path}'.")
 
+        # Get the duration of the audio file
+        wav_obj = wave.open(audio_file_path, "rb")
+        duration = wav_obj.getnframes() / float(wav_obj.getframerate())
+
         # Generate the `files` dictionary
         files = {
             "audio": open(audio_file_path, "rb"),
@@ -227,24 +252,55 @@ class Gentle:
         }
 
         # Create the progress bar task
-        progress_bar_task = asyncio.create_task(self._update_progress_bar(refresh_interval=refresh_interval))
+        progress_bar_task = asyncio.create_task(self._update_progress_bar(duration, refresh_interval=refresh_interval))
 
         # Create the file passing task
         async def __request_post_helper():
+            """Helper method to assist with the retrieval of the timetable."""
             async with ClientSession() as session:
-                return await session.post(url="http://localhost:8765/transcriptions?async=false", data=files)
+                async with session.post(url="http://localhost:8765/transcriptions?async=false", data=files) as response:
+                    return await response.json()
 
         file_passing_task = asyncio.create_task(__request_post_helper())
 
-        # Wait for completion of both tasks
-        response = await file_passing_task
+        # Wait for completion of the file processing task
+        try:
+            timetable_json = await file_passing_task
+        except ClientResponseError:
+            raise Exception("Something went wrong on the gentle server.")
+
+        # Await the completion of the progress bar task
         await progress_bar_task
 
-        # Check the response code
-        response.raise_for_status()
-        print(response.text)
+        # Return the timetable
+        return timetable_json
 
-        # Todo: process the timetable
+    def _get_raw_timetable(self, audio_file_path, transcript_path, refresh_interval=0.5):
+        """
+        Helper method that gets the raw timetable from the gentle server.
+
+        Args:
+            audio_file_path (str):
+                Path to the audio file.
+
+            transcript_path (str):
+                Path to the transcript.
+
+            refresh_interval (float):
+                Duration in seconds to wait before refreshing the progress bar.
+                (Default = 0.1)
+
+        Returns:
+            dict:
+                The timetable.
+
+        Raises:
+            FileNotFoundError:
+                If either the audio file or the transcript cannot be found.
+        """
+
+        return asyncio.run(self._get_raw_timetable_async(audio_file_path, transcript_path,
+                                                         refresh_interval=refresh_interval))
 
 
 # TESTING CODE
@@ -256,7 +312,8 @@ if __name__ == "__main__":
     gentle.start_gentle_container()
 
     # Try passing it an audio file and a transcript
-    gentle.get_timetable("../../TestTranscript.wav", "../../TranscriptClean.txt")
+    timetable = gentle.get_timetable("../../TestTranscript.wav", "../../TranscriptClean.txt")
+    print(timetable)
 
     # Stop the gentle container
     gentle.stop_gentle_container()
